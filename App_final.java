aa991d13028165d1bde2d5afd850d5f7f2548e8b
gennaro-97 <58686645+gennaro-97@users.noreply.github.com>
2025-03-03 15:24:13 +0100

    login-registrazione

--- App_final.java
+++ App_final.java
@@ -1,17 +1,62 @@
+import java.util.ArrayList;
+
 public class App_final {
     public static void main(String[] args) {
-        
+
+        // creo un menu login e registrazione
+        //
     }
 }
 
-//classe Utente
-class Utente{
+// classe Utente
+class Utente {
     String nome;
-    String password; 
+    String password;
 
-    public Utente(String nome, String password){
+    public Utente(String nome, String password) {
         this.nome = nome;
         this.password = password;
     }
 
+    @Override
+    public String toString() {
+        return "Nome: " + nome + " Password: " + password;
+    }
+
+}
+
+class Registro_Utenti {
+    ArrayList<Utente> utenti = new ArrayList<Utente>();
+
+    public void addUtente(Utente u) {
+        utenti.add(u);
+    }
+
+    public void stampa() {
+        for (Utente u : utenti) {
+            System.out.println(u);
+        }
+    }
+
+    public void registrazione(String nome, String password) {
+        // controllo se l'utente è già registrato
+        for (Utente u : utenti) {
+            if (u.nome.equals(nome)) {
+                System.out.println("Utente già registrato");
+                return;
+            }
+        }
+        Utente u = new Utente(nome, password);
+        utenti.add(u);
+        System.out.println("Utente registrato");
+    }
+
+    public boolean login(String nome, String password) {
+        for (Utente u : utenti) {
+            if (u.nome.equals(nome) && u.password.equals(password)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
